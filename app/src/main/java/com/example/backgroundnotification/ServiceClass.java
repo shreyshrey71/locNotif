032e4a9400f4cf52204522b61b35d319b380121c
shreyshrey71 <shreyshrey71@github.com>
2020-01-25 18:46:30 +0530

    [FEAT]Add Location nottification

--- app/src/main/java/com/example/backgroundnotification/ServiceClass.java
+++ app/src/main/java/com/example/backgroundnotification/ServiceClass.java
@@ -1,41 +1,100 @@
 package com.example.backgroundnotification;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.util.Log;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
 
 import static com.example.backgroundnotification.App.CHANNEL_ID;
 import static com.example.backgroundnotification.MainActivity.INTENT_TEXT;
 
-public class ServiceClass extends Service {
+public class ServiceClass extends Service implements LocationListener {
+    protected LocationListener locationListener;
+
+    LocationManager locationManager;
+    String loc = "";
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onCreate() {
         super.onCreate();
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+
 
         // will be called once
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        String input = intent.getStringExtra(INTENT_TEXT);
+    public void onLocationChanged(Location location) {
+        loc = "Latitude : "+ location.getLatitude() + " Longitude : "+ location.getLongitude();
+        Log.e("Location", loc);
+        sendNotification(loc);
 
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+
+    void sendNotification(String s)
+    {
         Intent notificationIntent = new Intent(this, MainActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,
                 0, notificationIntent, 0);
 
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setContentTitle("Example Service")
-                .setContentText(input)
+                .setContentText(loc)
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
                 .setContentIntent(pendingIntent)
                 .build();
 
         startForeground(1, notification);
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        String input = intent.getStringExtra(INTENT_TEXT);
+
+
+        sendNotification("");
 
         //do heavy work on a background thread
 
